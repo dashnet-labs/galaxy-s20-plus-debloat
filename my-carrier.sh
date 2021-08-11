239d5655574bd7b4b76f9d15e4e9e6279d18c166
dashnet-labs <87144292+dashnet-labs@users.noreply.github.com>
2021-08-11 08:23:12 -0700

    Create my-carrier.sh

---
+++ my-carrier.sh
@@ -0,0 +1,16 @@
+# My personal carrier script
+
+pm uninstall --user 0 com.att.android.attsmartwifi            
+pm uninstall --user 0 com.att.callprotect                     
+pm uninstall --user 0 com.att.csoiam.mobilekey                
+pm uninstall --user 0 com.att.dh                              
+pm uninstall --user 0 com.att.iqi                             
+pm uninstall --user 0 com.att.mobilesecurity                  
+pm uninstall --user 0 com.att.myWireless                      
+pm uninstall --user 0 com.att.personalcloud                   
+pm uninstall --user 0 com.att.tv                              
+pm uninstall --user 0 com.dti.att                             
+pm uninstall --user 0 com.sec.android.app.ewidgetatt          
+pm uninstall --user 0 com.synchronoss.dcs.att.r2g             
+pm uninstall --user 0 com.vzw.apnlib                          
+pm uninstall --user 0 net.aetherpal.device        
